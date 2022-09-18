603f93a5d5daa64570233d1d58597101c89accf2
EmilyCosta1420 <113480636+EmilyCosta1420@users.noreply.github.com>
2022-09-18 20:12:14 -0300

    Questão 23
    Realização da questão 23, vai mostra o número inteiro, a parte fracionária e o arredondamento do número.

--- Questão23/src/principal/principal.java
+++ Questão23/src/principal/principal.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class principal{
 
 	public static void main(String[] args) {
-	float real; 
-	int i;
-	float f, a;
-	Scanner numero = new
-    Scanner(System.in);
-	System.out.println("Digite um número real: ");
-	real = numero.nextFloat();
-	i = Math.round(real);
-	f = real - i;
+	Scanner sc = new Scanner(System.in);
+	double real, i,f, a;
+	System.out.println("Digite um número: ");
+	real = sc.nextDouble();
+	i = (int) real;
+	f = (float) real - i;
 	a = Math.round(real);
-	/*System.out.println("A parte inteira de" + real + " é: " + i);
-	System.out.println("A parte fracionária de" + real +"é:" + f);
-	System.out.println("O arredondamento de" + real + " é:" + a);*/
+	System.out.format("A parte inteira do número é: %.0f",i);
+	System.out.format("A parte fracionária do número é: %.2f",f);
+	System.out.println("A parte inteira do número é:" + a);
+		
 	}
 }
+
