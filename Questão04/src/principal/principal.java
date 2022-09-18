e3488988a0d8c25ddf1c402db2cc69415104a9cc
EmilyCosta1420 <113480636+EmilyCosta1420@users.noreply.github.com>
2022-09-18 11:13:28 -0300

    Questão 04
    Realização da questão 04, a questão 04, ela vai receber o salário do funcionário, calcula e mostra de novo o salário mais com um aumento de 25%.

--- Questão04/src/principal/principal.java
+++ Questão04/src/principal/principal.java
@@ -1,5 +1,12 @@
 package principal;
-
+import javax.swing.JOptionPane;
 public class principal {
 
+	public static void main(String[] args) {
+		int sal,salN; 
+		String nome = JOptionPane.showInputDialog("Insira seu nome.");
+		sal = Integer.parseInt(JOptionPane.showInputDialog("Bem vindo, " + nome + "! Agora insira seu salário atual."));
+		salN = sal + sal * 25/100;
+		System.out.println("O sistema indica que seu desempenho é alto, parabéns! Você ganhou um aumento de 25%. Seu salário agora é: " + salN);
+	}
 }
