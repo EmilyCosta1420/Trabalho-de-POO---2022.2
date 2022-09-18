68f1166e12a7ff8e55b5c371f38f346fa6fbf7a1
EmilyCosta1420 <113480636+EmilyCosta1420@users.noreply.github.com>
2022-09-18 11:24:22 -0300

    Questão 14
    Realização da questão 14, foi feito um programa pra saber sua idade atual e quanto você terá em 2035.

--- Questão14/src/principal/principal.java
+++ Questão14/src/principal/principal.java
@@ -1,5 +1,13 @@
 package principal;
-
+import javax.swing.JOptionPane;
 public class principal {
 
+	public static void main(String[] args) {
+		int anoAt, anoNsc, idAt, id35;
+		anoNsc = Integer.parseInt(JOptionPane.showInputDialog("Em que ano você nasceu?"));
+		anoAt = Integer.parseInt(JOptionPane.showInputDialog("So para ter certeza, em que ano você está?"));
+		idAt = anoAt - anoNsc;
+		id35 = 2035 - anoNsc;
+		System.out.println("Uau! Então você tem " + idAt + "! Mas você sabia que em 2035 você estará com: " + id35);
+	}
 }
